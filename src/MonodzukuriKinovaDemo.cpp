2eaeaf2a317112d8f3aefaba36faef65e61c12e8
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2024-10-25 18:25:05 +0900

    Add dynamics velocity damper

--- src/MonodzukuriKinovaDemo.cpp
+++ src/MonodzukuriKinovaDemo.cpp
@@ -1,8 +1,21 @@
 #include "MonodzukuriKinovaDemo.h"
 
 MonodzukuriKinovaDemo::MonodzukuriKinovaDemo(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
-: mc_control::fsm::Controller(rm, dt, config)
+: mc_control::fsm::Controller(rm, dt, config, Backend::TVM)
 {
+  // Initialize the velocity damper parameters (closed-loop by default)
+  dt_ = dt;
+  xsiOff_ = 0.0;
+  m_ = 2.0;
+  lambda_ = 100.0;
+  velocityDamperFlag_ = true;
+  closeLoopVelocityDamper_ = true;
+
+  // Initialize the constraints
+  selfCollisionConstraint->setCollisionsDampers(solver(), {m_, lambda_});
+  dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(
+    new mc_solver::DynamicsConstraint(robots(), 0, {0.1, 0.01, xsiOff_, m_, lambda_}, 0.9, true));
+  solver().addConstraintSet(dynamicsConstraint);
 
   mc_rtc::log::success("MonodzukuriKinovaDemo init done ");
 }
@@ -15,4 +28,53 @@ bool MonodzukuriKinovaDemo::run()
 void MonodzukuriKinovaDemo::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::fsm::Controller::reset(reset_data);
+}
+
+void MonodzukuriKinovaDemo::updateConstraints(bool closeLoop)
+{
+  if(closeLoop)
+  {
+    solver().removeConstraintSet(dynamicsConstraint);
+    dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(
+    new mc_solver::DynamicsConstraint(robots(), 0, {0.1, 0.01, xsiOff_, m_, lambda_}, 0.9, true));
+    solver().addConstraintSet(dynamicsConstraint);
+    selfCollisionConstraint->setCollisionsDampers(solver(), {m_, lambda_});
+
+    mc_rtc::log::info("[RALExpController] Close Loop Velocity damper is enabled");
+  }
+  else
+  {
+    solver().removeConstraintSet(dynamicsConstraint);
+    dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(
+      new mc_solver::DynamicsConstraint(robots(), 0, dt_, {0.1, 0.01, 0.5}, 0.9, false, true));
+    solver().addConstraintSet(dynamicsConstraint);
+    selfCollisionConstraint->setCollisionsDampers(solver(), {0.0, 0.0});
+
+    mc_rtc::log::info("[RALExpController] Close Loop Velocity damper is deactivated");
+  }
+}
+
+void MonodzukuriKinovaDemo::updateConstraints(void)
+{
+    if(m_ < 1.0 || lambda_ < 1.0)
+    {
+      solver().removeConstraintSet(dynamicsConstraint);
+      dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(
+        new mc_solver::DynamicsConstraint(robots(), 0, dt_, {0.1, 0.01, 0.5}, 0.9, false, true));
+      solver().addConstraintSet(dynamicsConstraint);
+      selfCollisionConstraint->setCollisionsDampers(solver(), {0.0, 0.0});
+      velocityDamperFlag_ = false;
+      closeLoopVelocityDamper_ = false;
+    }
+    else // Close loop velocity damper
+    {
+      solver().removeConstraintSet(dynamicsConstraint);
+      dynamicsConstraint = mc_rtc::unique_ptr<mc_solver::DynamicsConstraint>(
+        new mc_solver::DynamicsConstraint(robots(), 0, {0.1, 0.01, xsiOff_, m_, lambda_}, 0.9, true));
+      solver().addConstraintSet(dynamicsConstraint);
+      selfCollisionConstraint->setCollisionsDampers(solver(), {m_, lambda_});
+      velocityDamperFlag_ = true;
+      closeLoopVelocityDamper_ = true;
+    }
+    mc_rtc::log::info("[RALExpController] Constraints updated");
 }
--- src/MonodzukuriKinovaDemo.h
+++ src/MonodzukuriKinovaDemo.h
@@ -4,6 +4,9 @@
 
 #include "api.h"
 
+#define HIGH_RESIDUAL_GAIN 10.0
+#define LOW_RESIDUAL_GAIN 0.5
+
 struct MonodzukuriKinovaDemo_DLLAPI MonodzukuriKinovaDemo : public mc_control::fsm::Controller
 {
   MonodzukuriKinovaDemo(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
@@ -12,6 +15,24 @@ struct MonodzukuriKinovaDemo_DLLAPI MonodzukuriKinovaDemo : public mc_control::f
 
   void reset(const mc_control::ControllerResetData & reset_data) override;
 
+  // Update the Dynamics and the Collisions constraints for the controller
+  void updateConstraints(bool closeLoop); 
+  void updateConstraints(void);
+
+  // Dynamics velocity damper parameters
+  double m_;
+  double lambda_;
+  bool closeLoopVelocityDamper_;
+
 private:
   mc_rtc::Configuration config_;
+
+  // Dynamics velocity damper private parameters
+  double dt_;
+  bool velocityDamperFlag_;
+  double xsiOff_;
+  const double i_ = 0.03;
+  const double s_ = 0.015;
+  const double d_ = 0.0;
+
 };
